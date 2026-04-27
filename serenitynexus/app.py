a2d8295574fbd8ff85247e11b3a8c451a152fc65
serenity sas bic <serenitysasbic@gmail.com>
2026-04-27 15:37:22 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- serenitynexus/app.py
+++ serenitynexus/app.py
@@ -477,9 +477,9 @@ def footer():
     st.markdown(
         """
         <div style='text-align: center; color: grey; font-size: 0.8em;'>
-            <p>© 2026 <b>Serenity S.A.S. BIC</b> - Todos los derechos reservados.</p>
+            <p>© 2026 <b>Serenity Nexus Global</b> - Todos los derechos reservados.</p>
             <p>Software registrado ante la <b>DNDA</b>. Propiedad Intelectual Protegida.</p>
-            <p>Dagua, Valle del Cauca, Colombia.</p>
+            <p>Cali, Valle del Cauca, Colombia.</p>
         </div>
         """, 
         unsafe_allow_html=True
@@ -489,7 +489,7 @@ def footer():
         st.write("""
         Todo el contenido, algoritmos y estructuras de datos de Serenity Nexus Global 
         están protegidos por las leyes de derecho de autor. El uso no autorizado de 
-        la arquitectura 'Puntos Faro' o la marca Serenity S.A.S. BIC será objeto 
+        la arquitectura 'Puntos Faro' o la marca Serenity Nexus Global será objeto 
         de acciones legales pertinentes.
         """)
 
